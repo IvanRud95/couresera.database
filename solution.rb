25386264bc6d4b00b41b0ee9fbdd4811244e6f0d
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-25 14:38:11 +0300

    Create solution.rb

---
+++ solution.rb
@@ -0,0 +1,7 @@
+require_relative "solving_script"
+
+solution = Solution.new
+
+solution.analyze_file
+solution.calculate_line_with_highest_frequency
+solution.print_highest_word_frequency_across_lines
